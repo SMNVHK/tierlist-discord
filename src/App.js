5228de9b70f76d647cd9a90c69ce53c91edf538d
Simon Vanhemelryck <pepperpingu@gmail.com>
2024-08-05 21:50:47 +0200

    interactivity

--- src/App.js
+++ src/App.js
@@ -1,40 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
-import firebase from 'firebase/app';
-import 'firebase/database';
-import './TierList.css';
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getDatabase, ref, onValue, set } from "firebase/database";
+import './TierList.css';
 
 const firebaseConfig = {
-
   apiKey: "AIzaSyDCt4bLWo8OVYb7dO5Cjyin6VKa9czjuoo",
-
   authDomain: "bot-discord-c13ff.firebaseapp.com",
-
   databaseURL: "https://bot-discord-c13ff-default-rtdb.europe-west1.firebasedatabase.app",
-
   projectId: "bot-discord-c13ff",
-
   storageBucket: "bot-discord-c13ff.appspot.com",
-
   messagingSenderId: "276543551497",
-
   appId: "1:276543551497:web:882ebf4a639941e77da0d8",
-
   measurementId: "G-854G1PH0RF"
-
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-}
-
-const database = firebase.database();
+const database = getDatabase(app);
 
 const TierList = () => {
   const [items, setItems] = useState({
@@ -49,29 +32,23 @@ const TierList = () => {
   });
 
   useEffect(() => {
-    const itemsRef = database.ref('items');
-    itemsRef.on('value', (snapshot) => {
+    const itemsRef = ref(database, 'items');
+    onValue(itemsRef, (snapshot) => {
       const data = snapshot.val();
       if (data) {
         setItems(data);
       }
     });
-
-    return () => {
-      itemsRef.off();
-    };
   }, []);
 
   const onDragEnd = (result) => {
     if (!result.destination) return;
-
     const { source, destination } = result;
     const newItems = { ...items };
     const [reorderedItem] = newItems[source.droppableId].splice(source.index, 1);
     newItems[destination.droppableId].splice(destination.index, 0, reorderedItem);
-
     setItems(newItems);
-    database.ref('items').set(newItems);
+    set(ref(database, 'items'), newItems);
   };
 
   const tierColors = {
